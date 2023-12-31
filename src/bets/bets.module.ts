5d675befc4bdf92cc37153d3c21ad9d2cca20c3c
matthieu debarge <debarge.matthieu@gmail.com>
2023-12-31 19:53:07 +0100

    on relie un pari à un match et le pari consiste simplement à dire si l'équipe hôte gagne le match ou pas dans un booléen.

--- src/bets/bets.module.ts
+++ src/bets/bets.module.ts
@@ -4,9 +4,14 @@ import { BetsController } from './bets.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Bet } from './entities/bet.entity';
 import { User } from '../users/entities/user.entity';
+import { Game } from '../games/entities/game.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Bet]), TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([Bet]),
+    TypeOrmModule.forFeature([User]),
+    TypeOrmModule.forFeature([Game]),
+  ],
   controllers: [BetsController],
   providers: [BetsService],
 })
